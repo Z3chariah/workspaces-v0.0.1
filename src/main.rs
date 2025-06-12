5322e43953498d09ec3a8dbcc1f11471780a9a12
Elijah Powell <elijahpowell44@gmail.com>
2025-06-12 16:36:29 -0700

    refactor, general workspace state.

--- src/main.rs
+++ src/main.rs
@@ -1,108 +1,58 @@
+// use crossterm::Command;
 use crossterm::terminal::{disable_raw_mode, enable_raw_mode};
-use std::io::{self, Read, Write};
+use std::io::{self, Read};
 
 use std::process::exit;
 
 #[allow(unused_assignments)]
 #[allow(unused)]
 fn main() {
-    let mut current_workspace: Workspaces = Workspaces::Default;
-    let version = String::from("V0.0.1");
-    println!(
-        "Welcome to Workspaces version {}.\n Press [k] for the Study Space [j] for the game development workspace.",
-        version
-    );
-
-    while current_workspace != Workspaces::Exit {
+    loop {
         enable_raw_mode().unwrap();
         for b in io::stdin().bytes() {
             let keypress = b.unwrap() as char;
 
-            match keypress {
-                'k' => current_workspace = Workspaces::Study,
-                'j' => current_workspace = Workspaces::Gamedev,
-                'l' => current_workspace = Workspaces::Plugins,
-                'h' => current_workspace = Workspaces::Default,
-                'q' => current_workspace = Workspaces::Exit,
+            let mut ws: workspaces = match keypress {
+                'k' => workspaces::Study,
+                'j' => workspaces::Gamedev,
+                'l' => workspaces::Plugins,
+                'h' => workspaces::Default,
                 _ => {
                     disable_raw_mode().unwrap();
-                    exit(2);
+                    exit(1)
                 }
-            }
-
-            if current_workspace == Workspaces::Exit {
-                exit(2)
-            } else {
-                println!("\x1B[2J\x1B[H");
-                handleworkspace(&current_workspace);
-                continue;
-            }
+            };
+            print!("\x1B[2J\x1B[H");
+            ws.state_machine();
         }
     }
 }
 
-#[allow(unused)]
-fn handleworkspace(ws: &Workspaces) {
-    match ws {
-        Workspaces::Study => {
-            println!("Press [K] to open Obsidian [J] to open Remnote. and [L] to open Notion");
-            io::stdout().flush().unwrap();
-            use std::process::Command;
-
-            for b in io::stdin().bytes() {
-                let keypress = b.unwrap() as char;
-
-                match keypress {
-                    'k' => {
-                        Command::new("Open").arg("-a").arg("Obsidian").spawn();
-                    }
-                    'j' => {
-                        Command::new("Open").arg("-a").arg("RemNote").spawn();
-                    }
-
-                    'l' => {
-                        Command::new("Open").arg("-a").arg("Notion").spawn();
-                    }
-                    _ => handleworkspace(&Workspaces::Exit),
-                }
-            }
-        }
-
-        Workspaces::Gamedev => {
-            println!("Press [K] to open Godot [J] to open Aseprite. and [L] to open Zed");
-            use std::process::Command;
-            for b in io::stdin().bytes() {
-                let keypress = b.unwrap() as char;
-
-                match keypress {
-                    'k' => {
-                        Command::new("Open").arg("-a").arg("Godot").spawn();
-                    }
-                    'j' => {
-                        Command::new("Open").arg("-a").arg("Aseprite").spawn();
-                    }
-
-                    'l' => {
-                        Command::new("Open").arg("-a").arg("Zed").spawn();
-                    }
-                    _ => handleworkspace(&Workspaces::Exit),
-                }
-            }
-        }
-
-        Workspaces::Plugins => println!("inside of the plugins workspace."),
-        Workspaces::Default => println!("inside of the default workspace."),
-        Workspaces::Exit => {
-            exit(1);
-        }
-    }
-}
-
-#[derive(PartialEq)]
-enum Workspaces {
+#[allow(non_camel_case_types)]
+enum workspaces {
     Default,
     Study,
     Plugins,
     Gamedev,
-    Exit,
+}
+
+#[allow(unused)]
+impl workspaces {
+    fn state_machine(&self) {
+        match self {
+            workspaces::Study => {
+                println!("I am in the Study Workspace")
+            }
+            workspaces::Plugins => {
+                println!("I am in the Plugins Workspace")
+            }
+            workspaces::Default => {
+                println!("I am in the Default Workspace")
+            }
+            workspaces::Gamedev => {
+                println!("I am in the Game Developer Workspace")
+            }
+            _ => exit(0),
+        };
+    }
 }
